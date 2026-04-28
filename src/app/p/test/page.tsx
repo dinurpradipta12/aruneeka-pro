4dd919f105aad25f1e51c6f2df3e2b21f9c494aa
Dinur Pradipta <dinurm.pradipta@gmail.com>
2026-04-28 20:03:40 +0800

    chore: remove public preview feature as requested

--- src/app/p/test/page.tsx
+++
@@ -1,9 +0,0 @@
-// Halaman statis murni (tanpa Edge runtime) untuk pengujian isolasi total
-export default function TestPage() {
-  return (
-    <div style={{ padding: '100px', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1 style={{ fontSize: '48px', fontWeight: '900' }}>Static Test Success</h1>
-      <p>Jika Anda melihat ini, berarti sistem folder /p/ bekerja untuk halaman statis.</p>
-    </div>
-  );
-}
